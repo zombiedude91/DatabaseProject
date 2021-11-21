40f3fe66fdfe49c8627854992c05caed699b7ff3
Apichayalux Chanachaidham <apichayalux@gmail.com>
2021-11-22 01:06:37 +0700

    Update home.php

--- home.php
+++ home.php
@@ -102,7 +102,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-xl-9 mx-auto">
-                    <h1 class="mb-5" style="color:white;">Promotion</h1>
+                    <h1 class="mb-5" style="color:white;">Welcome to our home service, Friendly-Neighborhood</h1>
                 </div>
                 <div class="col-md-10 col-lg-8 col-xl-7 mx-auto">
                     <form action="shopfilter.php" method="POST" role="form">
