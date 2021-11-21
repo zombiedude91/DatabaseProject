94f5a3acd0d2b026c37781fe53521f723b4e9719
zombiedude91 <newnack2013@gmail.com>
2021-11-21 17:17:53 +0700

    Update editservice.php

--- editservice.php
+++ editservice.php
@@ -84,7 +84,7 @@
                             </div>
                         </div>
                         <div class="card-body">
-                            <form class="validate-form" action="updateservice.php" method="POST">
+                            <form class="validate-form" action="updateservice.php?id=<?php echo $_GET["id"] ?>" method="POST">
                                 <h6 class="heading-small text-muted mb-4"></h6>
                                 <div class="pl-lg-4">
                                     <div class="row">
